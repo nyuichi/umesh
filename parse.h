ab91ba0cc7063e585aca97b8aa6f46444865e5db
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-05-17 22:37:31 +0900

    reindent parse.*

--- parse.h
+++ parse.h
@@ -9,20 +9,20 @@
 #define LINELEN 256
 
 typedef enum write_option_ {
-    TRUNC,
-    APPEND,
+  TRUNC,
+  APPEND,
 } write_option;
 
 typedef struct process_ {
-    char*        program_name;
-    char**       argument_list;
+  char*        program_name;
+  char**       argument_list;
 
-    char*        input_redirection;
+  char*        input_redirection;
 
-    write_option output_option;
-    char*        output_redirection;
+  write_option output_option;
+  char*        output_redirection;
 
-    struct process_* next;
+  struct process_* next;
 
   /* runtime attribute */
   pid_t pid;
@@ -32,21 +32,21 @@ typedef struct process_ {
 } process;
 
 typedef enum job_mode_ {
-    FOREGROUND,
-    BACKGROUND,
+  FOREGROUND,
+  BACKGROUND,
 } job_mode;
 
 typedef struct job_ {
-    job_mode     mode;
-    process*     process_list;
-    struct job_* next;
+  job_mode     mode;
+  process*     process_list;
+  struct job_* next;
 } job;
 
 typedef enum parse_state_ {
-    ARGUMENT,
-    IN_REDIRCT,
-    OUT_REDIRCT_TRUNC,
-    OUT_REDIRCT_APPEND,
+  ARGUMENT,
+  IN_REDIRCT,
+  OUT_REDIRCT_TRUNC,
+  OUT_REDIRCT_APPEND,
 } parse_state;
 
 char* get_line(char *, int);
